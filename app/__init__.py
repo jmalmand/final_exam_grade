926cba33ad5bffda82ed5261ce6006a335929d94
cjtessler <codytessler@gmail.com>
2018-07-06 11:57:57 -0400

    Implemented complete form logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from config import Config
-from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-Bootstrap(app)
 app.config.from_object(Config)
 
 from app import routes
