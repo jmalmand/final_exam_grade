9d4477e5404f356584f6287aa6a9ee0ddd0b3790
cjtessler <codytessler@gmail.com>
2019-02-13 22:59:29 -0500

    Updated layout

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "a77429ef7b1d9036f82e246df921edea"
+    DEBUG = True
+    TESTING = True
+    ENV = False
